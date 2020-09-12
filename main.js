16c8f35a54122567cd919c79fdfd4d148d908114
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2020-09-12 19:00:29 +0100

    USSD updated

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 
 
-// function  mathCaculation(a,b){
-//   message = prompt("Enter the value you want to calculate")
-//   message = parseInt(message)
-//   console.log(a-b);
-//  result = (a-b)
-// }
-// mathCaculation(10,5);
-// alert('Your answer is: '+ result);
 
 
 // Simple USSD SERVICES
@@ -17,12 +9,7 @@
 2. Airtime-Self
 3. Airtime-Others
 4. Data
-5. Transfer to Sterling
-6. Transfer to Other banks
-7. Account Balance
-8. Open Account 
-9. My Account No(s)
-0. Next`);
+5. Transfer`);
 
 message = parseInt(message);
 
@@ -47,20 +34,22 @@ if (message == 1) {
     prompt(`Select: \n 1: Self 2: Others`);
     message = parseInt(message)
     let amount = prompt(`Select the Data Bundle of your choice: \n 1: 2gb - #1000 2: 5gb - #2000 3: 10gb - #4000 4: 40gb - #10000`);
-    if(message == 1){
-     
-      let amount = prompt("You are about to Recharge 2gb: Proceed?");
-      alert(`You've just recharged #${parseInt(amount)} Data`);
-  
-        // message = parseInt(message);
-    
-       }else if (message == 2) {
-         
-      
+    alert(`You've just recharged #${parseInt(amount)} Data`);
+ }else if (message == 5) {
+        let acct = prompt("Enter account number");
+        let bank = prompt(`Select your bank 1.Sterling Bank
+        2. UBA
+        3. GT
+        4. Access`);
+        let amount = prompt("Enter amount")
+        let ans = confirm(`Are you sure you want to send this ${parseInt(amount)} to ${acct}`);
+        if(ans == true){
+          alert(`you just transfered ${parseInt(amount)} to ${acct}`);
+        }
        
-    }
-    
-  }
+      }
+      
    
     
-    
+   
+  
